5cc8e2f7d87e129aae58f6f7382a4ec48463d1aa
ph_phich <ph_phich@hotmail.com>
2023-01-16 11:29:20 +0700

    Commit Client

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def abcd(request):
     return HttpResponse("Ha ha ha 555 ")
+
+def ClentSample(request):
+    return HttpResponse("Client")
