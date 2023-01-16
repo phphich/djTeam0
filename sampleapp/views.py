ba8e895d35cf4cde84169c2c2bfaf21274b21083
ph_phich <ph_phich@hotmail.com>
2023-01-16 11:26:39 +0700

    Master Up

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def abcd(request):
     return HttpResponse("Ha ha ha 555 ")
+
+
+def MasterSample(request):
+    return HttpResponse("Master")
